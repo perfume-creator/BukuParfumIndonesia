49bb13b8889c5e33ddaf335e3b0d97aedcdd1014
iqbalelbadra <iqbal.elbadra@mhs.unsoed.ac.id>
2026-02-20 01:29:34 +0700

    Add Vercel deployment config

---
+++ api/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// Autoload composer dependencies
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel application
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Run the application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
